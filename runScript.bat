7cb7ebae585c347b33bcd31dfdfc6870042b41c8
VERY-py <kirillzaytsev01@gmail.com>
2026-03-12 08:23:49 +0300

    Update runScript.bat

--- runScript.bat
+++ runScript.bat
@@ -3,7 +3,7 @@ chcp 65001 > nul
 setlocal enabledelayedexpansion
 
 echo ========================================
-echo     Запуск транслятора GoydaScript
+echo     Запуск компилятора GoydaScript
 echo ========================================
 echo.
 
